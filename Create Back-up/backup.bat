573e69ad84d7c1f80e246ed616bb9f1d53d96ac7
MahenM1988 <mmahindaratne88@gmail.com>
2024-10-03 00:49:13 +1000

    Update backup.bat

--- Create Back-up/backup.bat
+++ Create Back-up/backup.bat
@@ -4,15 +4,6 @@ setlocal
 set "SOURCE=C:\Users\Mahen Mahindaratne\OneDrive\Desktop\Source"
 set "DESTINATION=%~d0\Backup\Path"
 
-:: Calculate the size of the source directory
-set SIZE=0
-for /f "usebackq" %%A in (`dir /s /-c "%SOURCE%" ^| find "File(s)"`) do (
-    set SIZE=%%A
-)
-
-:: Remove commas for numerical comparison
-set SIZE=%SIZE:,=%
-
 :: Perform the backup
 robocopy "%SOURCE%" "%DESTINATION%" /E /Z /R:3 /W:5
 
